a5124696ba5c244bb5d40a71cd0065fb9bc7d1d0
yyuen <yale.yuen@mlb.com>
2017-03-17 15:14:36 -0700

    4

--- js/ClientApp.js
+++ js/ClientApp.js
@@ -2,19 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import MyTitle from './MyTitle'
 
-var div = React.DOM.div
-
-var MyTitleFactory = React.createFactory(MyTitle)
-
 var MyFirstComponent = React.createClass({
   render: function () {
     return (
-      div(null,
-        MyTitleFactory({title: 'props are the best', color: 'peru'}),
-        MyTitleFactory({title: 'semicolons are the worst', color: 'mediumaquamarine'}),
-        MyTitleFactory({title: 'jklol its okay if you like semicolons', color: 'rebeccapurple'}),
-        MyTitleFactory({title: 'im out of ideas', color: 'dodgerblue'})
-      )
+      <div>
+      	<MyTitle title="props are cool" color='rebeccapurple' />
+      	<MyTitle title="whatever" color='aquamarine' />
+      	<MyTitle title="yeh right" color='papayawhip' />
+      	<MyTitle title="no way" color='mediumaquamarine' />
+      	<MyTitle title="no way" color='peru' />
+      </div>
     )
   }
 })
